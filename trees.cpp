0a166b43df298ff25e30e16890644ec4727d3225
goelayush7 <ayushgoel95691@gmail.com>
2023-09-24 00:52:42 +0530

    files added

---
+++ trees.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+class Treenode{
+    public:
+        int val;
+        Treenode* left;
+        Treenode* right;
+        
+        Treenode(int val{
+            this->val=val;
+            this->left=this->right=NULL;
+        }
+};
+
+Treenode* buildtree(){
+    int val;
+    cin>>val;
+
+    Treenode* root = new Treenode(val);
+    root->left=
+}
+)
+int main () {
+
+	}
+	return 0;
+}
